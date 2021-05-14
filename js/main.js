162313258246a36e110b0ffc23eb0475f429f45f
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-05-14 18:34:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+// prevent text from being deleted
+// https://stackoverflow.com/questions/36318658/how-to-prevent-a-user-from-removing-the-first-three-characters-in-a-text-input
+// hack, needs to be replaced because user input cant be deleted
+$(document).on('keydown', function (e) {
+  if (e.keyCode == 8 && $('#Lab').is(":focus") && $('#Lab').val().length < 4) {
+      e.preventDefault();
+  }
+});
