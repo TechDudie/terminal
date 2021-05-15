b2759c949c97aafa7cbf2686301deb61bc0a4574
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-05-15 18:49:29 -0500

    Create commands.js

---
+++ js/commands.js
@@ -0,0 +1,14 @@
+function run(cmd) {
+  var tokens = cmd.split(" ");
+  var output = "";
+  switch (tokens[0]) {
+    case "pwd":
+      output = directory;
+      break;
+    case "cd":
+      directory = cd(directory,tokens[1]);
+      break;
+    default:
+      output = "Unknown command";
+  }
+}
