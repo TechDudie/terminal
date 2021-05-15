127452e2ed01abc87894586d5f54b292784fbd91
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-05-15 18:24:05 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-hostname = "terminal"
-username = "terminal"
-directory = "~"
+var hostname = "terminal";
+var username = "terminal";
+var directory = "~";
+var cmd = "";
 document.addEventListener("keydown",  e => {
   if (e.keyCode === 8)  {
     e.preventDefault();
-  }
-  if (e.keyCode === 13)  {
+  } else if (e.keyCode === 13)  {
     e.preventDefault();
+    alert(cmd); //debug
     command();
+    cmd = "";
+  } else {
+    cmd += String.fromCharCode(e.keyCode);
   }
 });
 setup();
