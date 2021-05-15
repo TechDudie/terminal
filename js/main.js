6e13aa823e3d67f2045e8e84ce0bbea485e2625a
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-05-15 16:26:21 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,15 +10,4 @@ document.addEventListener("keydown",  e => {
     command();
   }
 });
-function print(text) {
-  document.getElementById("terminal").value += text;
-}
-function command() {
-  var text = "\n" + username + "@" + hostname + " " + directory + " $ ";
-  document.getElementById("terminal").value += text;
-}
-function setup() {
-  var text = username + "@" + hostname + " " + directory + " $ ";
-  document.getElementById("terminal").value += text;
-}
 setup();
