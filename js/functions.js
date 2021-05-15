1b8f1b2e707eb397122d0788988e6660ff478347
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-05-15 16:26:30 -0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,11 @@
-
+function print(text) {
+  document.getElementById("terminal").value += text;
+}
+function command() {
+  var text = "\n" + username + "@" + hostname + " " + directory + " $ ";
+  document.getElementById("terminal").value += text;
+}
+function setup() {
+  var text = username + "@" + hostname + " " + directory + " $ ";
+  document.getElementById("terminal").value += text;
+}
